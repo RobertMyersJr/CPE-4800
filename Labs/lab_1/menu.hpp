9b45341ae95bc549085e8c7d4804cb2e07ae2872
Robertf2003 <robertjr.legend.electric@gmail.com>
2026-01-31 21:14:11 -0600

    NEED TO DO NUMBER

--- Labs/lab_1/menu.hpp
+++ Labs/lab_1/menu.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+#include <string_view>
 enum menus {
     MAIN_MENU,
     REGISTER_NEW_USER_MENU,
@@ -8,9 +10,11 @@ enum menus {
 };
 class Menu {
     public:
+        Menu(std::string_view current_user);
         menus display_menu(menus menu);
     private:
         menus display_main_menu();
         void register_newuser_menu();
         void change_password_menu();
+        std::string current_user_;
 };
